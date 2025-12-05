2b2b979cf2e79471af485cf3dc14045734feeb73
Nikita <troshin.nksr@gmail.com>
2025-12-05 22:55:43 +0300

    -

--- media_server/entrypoint.sh
+++ media_server/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-# Подставляем переменные в nginx.conf
 envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-# Запускаем nginx
 exec nginx -g 'daemon off;'
